8961197baa85dab80309722915569055e0bca213
bradawk <niepceron.brad@gmail.com>
2017-08-16 10:46:23 +0200

    Refacto

--- back-end/app/lib/randomCurve.js
+++ back-end/app/lib/randomCurve.js
@@ -7,8 +7,8 @@ function randomCurve(){
     var coefficient = Math.floor(Math.random() * (10 + 10) -10);
     var delta_curve = '';
     
-    curve.push({value:'sigmoid', params:{'lambda':(Math.random()*(0.04 - 0.01) + 0.01).toFixed(2), 'coef':(Math.random()*(80 - 20)+20).toFixed(2),'const':(Math.random()*(20-10)+10).toFixed(2),'delta':(Math.random()*(700-100)+100).toFixed(2)}})
-    curve.push({value:'sigmoid', params:{'lambda':(Math.random()*(0.04 - 0.01) + 0.01).toFixed(2), 'coef':(Math.random()*(-80)).toFixed(2),'const':(Math.random()*(15-0)+0).toFixed(2),'delta':(Math.random()*(2300-1900)+1900).toFixed(2)}})
+    curve.push({value:'sigmoid', params:{'lambda':(Math.random()*(0.02 - 0.0005) + 0.005).toFixed(2), 'coef':(Math.random()*(80 - 20)+20).toFixed(2),'const':(Math.random()*(20-10)+10).toFixed(2),'delta':(Math.random()*(700-100)+100).toFixed(2)}})
+    curve.push({value:'sigmoid', params:{'lambda':(Math.random()*(0.02 - 0.0005) + 0.005).toFixed(2), 'coef':(Math.random()*(-80)).toFixed(2),'const':(Math.random()*(15-0)+0).toFixed(2),'delta':(Math.random()*(2300-1900)+1900).toFixed(2)}})
 
     for(var i = 0; i < Math.floor(Math.random() * (10 - 2) + 2); i ++){
         curve.push({value: 'gaussian', params:{'sigma': (Math.random() * (200 - 20.0) + 20.0).toFixed(2), 'mu': (Math.random() * (2300 - 800) + 800).toFixed(2),'coef':(Math.random() * (3001) - 1500).toFixed(2)}});
--- front-end/src/components/forms/CurveUpdateForm.vue
+++ front-end/src/components/forms/CurveUpdateForm.vue
@@ -8,7 +8,7 @@
                   <label> Lag : </label>
                   <input v-model="lag" type="number" placeholder="Lag" disabled />
                   <label> Coefficient : </label>
-                  <input v-model="coefficient" type="number" placeholder="Coefficient" disabled/>
+                  <input v-model="coefficient" type="number" placeholder="Coefficient"/>
                 <div v-for="(c, index) in curve">
                   <div>
                     <h5> Curve {{index}} </h5>
